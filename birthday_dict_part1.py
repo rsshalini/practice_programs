780e5edc28f840b0783535ad10c459e0a5a655cf
ssubra <ssubra@juniper.net>
2018-07-30 21:20:43 -0700

    added prog

---
+++ birthday_dict_part1.py
@@ -0,0 +1,12 @@
+#! usr/bin/python
+
+birthday_dict = dict()
+birthday_dict = {"ravi":"1 Jan 1973", "omar": "5 Jul 1992", "julie":"11 Aug 2000"}
+
+print  "Welcome to the birthday dictionary. We have birth dates of:\n {0} \n  \n ".format(birthday_dict.keys)
+
+# for i in birthday_dict.keys():
+#     print i#
+
+print birthday_dict.keys()
+print i for i in birthday_dict.keys()
