28908c2aedadadeac819e6fee7a982e808c1e7e9
ssubra <ssubra@juniper.net>
2019-02-10 18:32:51 -0800

    added logging debug instead of print statement

--- pizza.py
+++ pizza.py
@@ -1,14 +1,19 @@
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+
 class Pizza():
     def __init__(self,name,price):
         self.name = name
         self.price = price
-        print "Pizza created: {} (${})".format(self.name, self.price)
+        logging.debug("Pizza created: {} (${})".format(self.name, self.price))
 
     def make(self, quantity = 1):
-        print "Made {} {} pizza(s)".format(quantity, self.name)
+        logging.debug("Made {} {} pizza(s)".format(quantity, self.name))
 
     def eat(self, quantity = 1):
-        print "Ate {} {} pizza(s)".format(quantity, self.name)
+        logging.debug("Ate {} {} pizza(s)".format(quantity, self.name))
 
 pizza_01 = Pizza("artichoke", 15)
 pizza_01.make()
